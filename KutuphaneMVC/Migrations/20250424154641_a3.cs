d012782dc14b7301e4476b8ca3ff0f903e4f660c
depo0749 <depo0749@gmail.com>
2026-05-02 14:37:09 +0300

    Add KutuphaneMVC/Migrations/20250424154641_a3.cs

---
+++ KutuphaneMVC/Migrations/20250424154641_a3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KutuphaneMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class a3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KitapTuruİd",
+                table: "kitaps");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KitapTuruİd",
+                table: "kitaps",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
