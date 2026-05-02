0e405af1a297179a503009f79731ef8adf0f64a9
depo0749 <depo0749@gmail.com>
2026-05-02 14:37:06 +0300

    Add KutuphaneMVC/Migrations/20250424153453_a2.cs

---
+++ KutuphaneMVC/Migrations/20250424153453_a2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KutuphaneMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class a2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "yoneticis");
+
+            migrationBuilder.RenameColumn(
+                name: "Aciklama",
+                table: "kitaps",
+                newName: "Tur");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tur",
+                table: "kitaps",
+                newName: "Aciklama");
+
+            migrationBuilder.CreateTable(
+                name: "yoneticis",
+                columns: table => new
+                {
+                    YId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    YAdi = table.Column<string>(type: "TEXT", nullable: true),
+                    YEposta = table.Column<string>(type: "TEXT", nullable: true),
+                    YKapakFoto = table.Column<string>(type: "TEXT", nullable: true),
+                    YSifre = table.Column<string>(type: "TEXT", nullable: true),
+                    YTelefon = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yoneticis", x => x.YId);
+                });
+        }
+    }
+}
